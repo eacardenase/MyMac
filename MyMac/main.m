a24b3534795d13933bbdd7592f2a788b584c44b5
Edwin Cardenas <eacardenase@gmail.com>
2024-12-31 12:55:29 -0500

    Initial Commit

---
+++ MyMac/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyMac
+//
+//  Created by Edwin Cardenas on 12/31/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
